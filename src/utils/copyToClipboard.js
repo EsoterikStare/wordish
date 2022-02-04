45769c413cb9f6d60b1bcdbce8b00342b799bec8
Erik Burton <erikbburton@gmail.com>
2022-02-03 23:32:47 -0700

    Try a different copy method to see if mobile works

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -1,7 +1,10 @@
 const copyToClipboard = (content, callback) => {
-  navigator.clipboard.writeText(content)
-    .then(() => callback(true))
-    .catch(() => callback(false));
+  // navigator.clipboard.writeText(content)
+  //   .then(() => callback(true))
+  //   .catch(() => callback(false));
+  content.select();
+  const result = document.execCommand('copy');
+  callback(result);
 };
 
 export default copyToClipboard;
