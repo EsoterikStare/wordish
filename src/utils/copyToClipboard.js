de8c31db506e0cf2350a4c4880b28fba97518b6d
Erik Burton <erikbburton@gmail.com>
2022-02-05 13:11:56 -0700

    Try detecting iOS and triggering share dialog

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -2,16 +2,53 @@ const copyToClipboard = (content, callback) => {
   let textarea;
   let result;
 
+  function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+  }
+
+  const iOSShare = async (data) => {
+    try {
+      await navigator.share(data);
+      result = true;
+    } catch (err) {
+      result = false;
+    }
+  };
+
+  if (iOS()) {
+    iOSShare({ text: content });
+    callback(result);
+    return;
+  }
+
+  // Have to append to the dialog since it's the only focusable layer when this fires.
   const dialogBackdrop = document.getElementsByClassName('MuiDialogContent-root')[0];
 
   try {
     textarea = document.createElement('textarea');
+
+    // readonly prevents the keyboard from popping up when
+    // the textfield focuses.
     textarea.setAttribute('readonly', true);
+    // iOS only allows copying from contenteditable fields
     textarea.setAttribute('contenteditable', true);
-    textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+    // prevent scroll from jumping to the bottom when focus is set.
+    textarea.style.position = 'fixed';
     textarea.value = content;
 
     dialogBackdrop.appendChild(textarea);
+
+    // console.log('textarea', textarea);
+    // debugger;
 
     textarea.focus();
     textarea.select();
@@ -31,7 +68,7 @@ const copyToClipboard = (content, callback) => {
     dialogBackdrop.removeChild(textarea);
   }
 
-  callback(result);
+  callback(!!result);
 };
 
 export default copyToClipboard;
