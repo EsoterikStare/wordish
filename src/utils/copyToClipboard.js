6efe05ad57275a75903e0cbd79a1aee5075ba33d
Erik Burton <erikbburton@gmail.com>
2022-02-05 13:19:00 -0700

    drop ios detection and always share to test

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -1,19 +1,19 @@
 const copyToClipboard = (content, callback) => {
-  let textarea;
+  // let textarea;
   let result;
 
-  function iOS() {
-    return [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
-    ].includes(navigator.platform)
-    // iPad on iOS 13 detection
-    || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
-  }
+  // function iOS() {
+  //   return [
+  //     'iPad Simulator',
+  //     'iPhone Simulator',
+  //     'iPod Simulator',
+  //     'iPad',
+  //     'iPhone',
+  //     'iPod'
+  //   ].includes(navigator.platform)
+  //   // iPad on iOS 13 detection
+  //   || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+  // }
 
   const iOSShare = async (data) => {
     try {
@@ -24,51 +24,50 @@ const copyToClipboard = (content, callback) => {
     }
   };
 
-  if (iOS()) {
-    iOSShare({ text: content });
-    callback(result);
-    return;
-  }
+  iOSShare({ text: content });
+  callback(result);
+  // if (iOS()) {
+  // }
 
-  // Have to append to the dialog since it's the only focusable layer when this fires.
-  const dialogBackdrop = document.getElementsByClassName('MuiDialogContent-root')[0];
+  // // Have to append to the dialog since it's the only focusable layer when this fires.
+  // const dialogBackdrop = document.getElementsByClassName('MuiDialogContent-root')[0];
 
-  try {
-    textarea = document.createElement('textarea');
+  // try {
+  //   textarea = document.createElement('textarea');
 
-    // readonly prevents the keyboard from popping up when
-    // the textfield focuses.
-    textarea.setAttribute('readonly', true);
-    // iOS only allows copying from contenteditable fields
-    textarea.setAttribute('contenteditable', true);
-    // prevent scroll from jumping to the bottom when focus is set.
-    textarea.style.position = 'fixed';
-    textarea.value = content;
+  //   // readonly prevents the keyboard from popping up when
+  //   // the textfield focuses.
+  //   textarea.setAttribute('readonly', true);
+  //   // iOS only allows copying from contenteditable fields
+  //   textarea.setAttribute('contenteditable', true);
+  //   // prevent scroll from jumping to the bottom when focus is set.
+  //   textarea.style.position = 'fixed';
+  //   textarea.value = content;
 
-    dialogBackdrop.appendChild(textarea);
+  //   dialogBackdrop.appendChild(textarea);
 
-    // console.log('textarea', textarea);
-    // debugger;
+  //   // console.log('textarea', textarea);
+  //   // debugger;
 
-    textarea.focus();
-    textarea.select();
+  //   textarea.focus();
+  //   textarea.select();
 
-    const range = document.createRange();
-    range.selectNodeContents(textarea);
+  //   const range = document.createRange();
+  //   range.selectNodeContents(textarea);
 
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
+  //   const selection = window.getSelection();
+  //   selection.removeAllRanges();
+  //   selection.addRange(range);
 
-    textarea.setSelectionRange(0, textarea.value.length);
-    result = document.execCommand('copy');
-  } catch (err) {
-    console.error(err);
-  } finally {
-    dialogBackdrop.removeChild(textarea);
-  }
+  //   textarea.setSelectionRange(0, textarea.value.length);
+  //   result = document.execCommand('copy');
+  // } catch (err) {
+  //   console.error(err);
+  // } finally {
+  //   dialogBackdrop.removeChild(textarea);
+  // }
 
-  callback(!!result);
+  // callback(!!result);
 };
 
 export default copyToClipboard;
