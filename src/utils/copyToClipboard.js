40f26348762303c264a0e175d50eaa9b836a0507
Erik Burton <erikbburton@gmail.com>
2022-02-04 21:40:37 -0700

    Add temp textarea to dialog so it can focus before copy

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -1,9 +1,36 @@
 const copyToClipboard = (content, callback) => {
-  // navigator.clipboard.writeText(content)
-  //   .then(() => callback(true))
-  //   .catch(() => callback(false));
-  content.select();
-  const result = document.execCommand('copy');
+  let textarea;
+  let result;
+
+  const dialogBackdrop = document.getElementsByClassName('MuiDialogContent-root')[0];
+
+  try {
+    textarea = document.createElement('textarea');
+    textarea.setAttribute('readonly', true);
+    textarea.setAttribute('contenteditable', true);
+    textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+    textarea.value = content;
+
+    dialogBackdrop.appendChild(textarea);
+
+    textarea.focus();
+    textarea.select();
+
+    const range = document.createRange();
+    range.selectNodeContents(textarea);
+
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    textarea.setSelectionRange(0, textarea.value.length);
+    result = document.execCommand('copy');
+  } catch (err) {
+    console.error(err);
+  } finally {
+    dialogBackdrop.removeChild(textarea);
+  }
+
   callback(result);
 };
 
