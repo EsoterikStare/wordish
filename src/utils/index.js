3911e6d547a83ef0c9686d942083e8f4fb88f8ae
Erik Burton <erikbburton@gmail.com>
2022-02-06 08:28:07 -0700

    Create useLocalStorage hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as copyToClipboard } from './copyToClipboard';
 export { default as getShareGrid } from './getShareGrid';
+export { default as useLocalStorage } from './useLocalStorage';
