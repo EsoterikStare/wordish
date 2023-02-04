eb0b675c4041004865abee6031e457dd29af52dc
Erik Burton <erikbburton@gmail.com>
2023-02-04 16:52:49 -0600

    Linting and add a documentation comment

--- src/words/words.js
+++ src/words/words.js
@@ -3,9 +3,11 @@ import sixLetters from './sixLetters';
 
 export const words = {
   5: fiveLetters,
-  6: sixLetters
+  6: sixLetters,
 };
 
+// Dynamically calculate the order of magnitude of the current word count to avoid an
+// arbitrarily large number to base our random index on.
 const getOrderOfMagnitudeLimit = (wordLength) => 10 ** words[wordLength].length.toString().length;
 export const getRandomIndex = (wordLength) => (
   Math.floor(Math.random() * getOrderOfMagnitudeLimit(wordLength))
