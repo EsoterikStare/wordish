d5eba9e0dc1d291692d6bba382b54f6ddb74b1ed
Erik Burton <erikbburton@gmail.com>
2023-02-06 14:56:04 -0600

    Add gameStates for testing; Remove expected share text; Update ShareButton tests

---
+++ src/test-utils/gameStates/index.js
@@ -0,0 +1,9 @@
+import loseGameStateJSON from './loseGameState.json';
+import midGuessGameStateJSON from './midGuessGameState.json';
+import newGameStateJSON from './newGameState.json';
+import winGameStateJSON from './winGameState.json';
+
+export const loseGameState = JSON.stringify(loseGameStateJSON);
+export const midGuessGameState = JSON.stringify(midGuessGameStateJSON);
+export const newGameState = JSON.stringify(newGameStateJSON);
+export const winGameState = JSON.stringify(winGameStateJSON);
