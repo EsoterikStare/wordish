c1520fc8f22661a60c2dfe4d0e3e109e4630a124
Erik Burton <erikbburton@gmail.com>
2023-02-06 14:57:56 -0600

    Update setup test-util to provide wrapper to rerender as well

--- src/test-utils/setup.js
+++ src/test-utils/setup.js
@@ -1,20 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { node } from 'prop-types'
 import React from 'react';
 
 import { AppStateProvider } from '../AppStateContext';
 import AppThemeProvider from '../AppThemeProvider';
 
-const setup = (jsx) => ({
-  user: userEvent.setup(),
-  ...render(
-    <AppStateProvider>
-      <AppThemeProvider>
-        {jsx}
-      </AppThemeProvider>
-    </AppStateProvider>,
-  ),
-});
+const Wrapper = ({ children }) => (
+  <AppStateProvider>
+    <AppThemeProvider>
+      {children}
+    </AppThemeProvider>
+  </AppStateProvider>
+);
+Wrapper.propTypes = {
+  children: node.isRequired,
+};
+Wrapper.defaultProps = {};
+
+const setup = (jsx) => {
+  const { rerender, ...rest } = render(<Wrapper>{jsx}</Wrapper>);
+  return {
+    user: userEvent.setup(),
+    rerender: newJsx => rerender(<Wrapper>{newJsx}</Wrapper>),
+    ...rest,
+  };
+};
 
 export default setup;
