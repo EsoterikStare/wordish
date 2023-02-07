fe238b5f7f4e5275e5b1167f3e839bd582df5251
Erik Burton <erikbburton@gmail.com>
2023-02-06 20:34:31 -0600

    Refactor mockLocalStorage to provide a cleanup function

--- src/test-utils/mockLocalStorage.js
+++ src/test-utils/mockLocalStorage.js
@@ -1,15 +1,31 @@
-const mockLocalStorage = (gameState = {}) => {
+import { newGameState } from './gameStates';
+
+const mockLocalStorage = (gameState = newGameState) => {
+  const originalLocalStorage = { ...window.localStorage };
+  const mockValues = {
+    clear: jest.fn(),
+    // Provide a known win state from localStorage mock that will be used by AppStateProvider.
+    getItem: jest.fn(() => gameState),
+    removeItem: jest.fn(),
+    setItem: jest.fn(),
+  };
+
   // Mock window.localStorage with assertable functions.
   Object.defineProperty(window, 'localStorage', {
-    value: {
-      clear: jest.fn(),
-      // Provide a known win state from localStorage mock that will be used by AppStateProvider.
-      getItem: jest.fn(() => gameState),
-      removeItem: jest.fn(),
-      setItem: jest.fn(),
-    },
+    value: mockValues,
     writable: true,
   });
+
+  const resetLocalStorage = () => {
+    Object.defineProperty(window, 'localStorage', {
+      value: originalLocalStorage,
+    });
+  };
+
+  return {
+    resetLocalStorage,
+    ...mockValues,
+  };
 };
 
 export default mockLocalStorage;
