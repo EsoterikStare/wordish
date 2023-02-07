cd8426fb0d0e17e43730b9910b007374efe4d327
Erik Burton <erikbburton@gmail.com>
2023-02-06 22:26:58 -0600

    Add Keyboard component tests

---
+++ src/components/Keyboard.test.jsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+
+import Keyboard from './Keyboard';
+
+import {
+  loseGameState,
+  mockLocalStorage,
+  newGameState,
+  setup,
+  winGameState,
+} from '../test-utils';
+
+describe('Keyboard', () => {
+  it('should be defined', () => {
+    expect(Keyboard).toBeDefined();
+  });
+  it('should render', () => {
+    setup(<Keyboard />);
+    expect(screen.getByTestId('keyboard-grid')).toBeInTheDocument();
+  });
+  it('should look the same for a new game state', () => {
+    const { resetLocalStorage } = mockLocalStorage(newGameState);
+    setup(<Keyboard />);
+    expect(screen.getByTestId('keyboard-grid')).toMatchSnapshot();
+    resetLocalStorage();
+  });
+  it('should look the same for a lose game state', () => {
+    const { resetLocalStorage } = mockLocalStorage(loseGameState);
+    setup(<Keyboard />);
+    expect(screen.getByTestId('keyboard-grid')).toMatchSnapshot();
+    resetLocalStorage();
+  });
+  it('should look the same for a win game state', () => {
+    const { resetLocalStorage } = mockLocalStorage(winGameState);
+    setup(<Keyboard />);
+    expect(screen.getByTestId('keyboard-grid')).toMatchSnapshot();
+    resetLocalStorage();
+  });
+});
