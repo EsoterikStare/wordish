89b632965c42d9afe80cc3410a7d0f2a4f414b06
Erik Burton <erikbburton@gmail.com>
2023-02-12 23:42:53 -0600

    Add a few more regression tests for WordGrid and update snapshot

--- src/components/WordGrid.test.jsx
+++ src/components/WordGrid.test.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 
 import WordGrid from './WordGrid';
 
-import { loseGameState, mockLocalStorage, setup } from '../test-utils';
+import {
+  loseGameState,
+  midGuessGameState,
+  mockLocalStorage,
+  setup,
+  winGameState,
+} from '../test-utils';
 
 describe('WordGrid', () => {
   it('should be defined', () => {
@@ -13,7 +19,26 @@ describe('WordGrid', () => {
     setup(<WordGrid />);
     expect(screen.getByTestId('words-grid')).toBeInTheDocument();
   });
-  it('should correctly arrange Word components in a vertical stack', () => {
+  it('should render 42 Letter components for a 6-letter-word game', () => {
+    mockLocalStorage(loseGameState);
+    setup(<WordGrid />);
+
+    expect(screen.getAllByTestId('letter-grid')).toHaveLength(42);
+  });
+  it('should render 30 Letter components for a 5-letter-word game', () => {
+    mockLocalStorage(winGameState);
+    setup(<WordGrid />);
+
+    expect(screen.getAllByTestId('letter-grid')).toHaveLength(30);
+  });
+  it('should still have the correct number of Letter components after user input', async () => {
+    mockLocalStorage(midGuessGameState);
+    const { user } = setup(<WordGrid />);
+    await user.keyboard('{Backspace}{Backspace}{Backspace}spoon');
+
+    expect(screen.getAllByTestId('letter-grid')).toHaveLength(30);
+  });
+  it('should look as expected', () => {
     mockLocalStorage(loseGameState);
     setup(<WordGrid />);
     expect(screen.getByTestId('words-grid')).toMatchSnapshot();
