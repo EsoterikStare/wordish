36b0e444acbcf4ba6998dcc872f4f28b8b1d71ff
Erik Burton <erikbburton@gmail.com>
2023-02-13 22:45:10 -0600

    Finish setting up Cypress Cloud recording

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'ddh3ro',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
